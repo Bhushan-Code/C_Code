23a48180722d1599439b62009cc1bd43e842a341
Bhushan-code <nagpureb7@gmail.com>
2024-08-20 21:39:28 +0530

    20-8-24 function and reccursion

--- C5_functions.c
+++ C5_functions.c
@@ -1,8 +1,4 @@
 /*
-Sytax 1 - Function Declaration / Prototype
-
-void printHello();
-
 
 
 Syntax 2 - Function Call
@@ -13,8 +9,6 @@ int main(){
     }
 
 
-
-
 Syntax 3 - Function Defination
 
 void printHello(){
@@ -22,10 +16,247 @@ void printHello(){
     }
 
 
+*/
 
 
 
 
-Syntax 4
+
+
+// for indian say namaste and for french say bonjour
+
+/*
+#include<stdio.h>
+
+void Namaste()
+{
+    printf("Namaste \n");
+}
+
+void Bonjoir()
+{
+    printf("Bonjoir \n");
+}
+
+int main()
+{
+   char ch;
+   printf("Please enter your nation (i or f): ");
+   scanf("%c", &ch);
+
+   if(ch == 'f')
+   {
+    Bonjoir();
+   }
+
+    else if(ch == 'i')
+    {
+        Namaste();
+    }
+   
+    else 
+    {
+        printf("enter valid country");
+    }
+   
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+//take two number and return sum of two numbers
+
+/*
+#include<stdio.h>
+
+
+int sum(int a, int b)
+{
+    return a+b;
+}
+
+
+int main()
+{
+    int a=0;
+    int b=0;
+
+    printf("Enter First Number : \n");
+    scanf("%d", &a);
+
+    printf("Enter Second Number : \n");
+    scanf("%d", &b);
+
+    int s = sum(a, b);
+    printf("Sum is : %d\n", s);
+
+    return 0;
+}
+*/
+
+
+
+
+//print table of given number
+
+/*
+
+#include<stdio.h>
+void table(int N)
+{
+    for(int i=1; i<=10; i++)
+    {
+    int sum = i * N;    
+    printf("%d\n", sum);    
+    }
+}
+
+
+int main()
+{
+    int N=0;
+    printf("Enter the number : ");
+    scanf("%d", &N);
+
+    table(N);
+    return 0;
+}
+*/
+
+
+
+
+
+//add 18% gst on any given price
+
+/*
+#include<stdio.h>
+
+float GST(float P)
+{
+  return P+(0.18*P);
+}
+
+void calvalue(float value)
+    {
+        value = value+(0.18*value);
+        printf("cal by using void %f\n", value);
+    }
+
+
+int main()
+{
+    float P=0;
+    float value=0;
+
+    printf("Enter Product Price : ");
+    scanf("%f", &P);
+
+    calvalue(P);                                         //type 1 by apana college
+    printf("cal by using float %f\n", GST(P));           //type 2 by bhushan
+
+}
 
 */
+
+
+
+
+
+
+//Reccursion
+//write a code to type hello world  by reccursion
+
+/*
+
+#include<stdio.h>
+
+void Hello(int N)
+{
+    if (N==0){
+        return;
+    }
+    printf("Hello World\n");
+    Hello(N-1);
+}
+
+int main()
+{
+    int N=0;
+    printf("How many times : ");
+    scanf("%d", &N);
+
+    Hello(N);
+}
+
+
+*/
+
+
+
+//Sum of first n natural number
+
+/*
+#include<stdio.h>
+
+int sum(int n)
+{   
+    if(n==1)
+    {
+        return 1;
+    }
+    int sumNm = sum(n-1);
+    int sumN = sumNm*n;
+    return sumN;
+}
+
+int main()
+{
+    int n=0;
+    printf("enter: ");
+    scanf("%d", &n);
+    printf("%d\n",sum(n));
+    return 0;
+}
+*/
+
+
+
+
+//factorial of given number
+/*
+#include<stdio.h>
+
+int fact(int n)
+{   
+    if(n==1)
+    {
+        return 1;
+    }
+    int factNm = fact(n-1);
+    int factN = factNm*n;
+    return factN;
+}
+
+int main()
+{
+    int n=0;
+    scanf("%d", &n);
+    printf("%d\n",fact(n));
+    return 0;
+}
+*/
+
+#include<stdio.h>
+int main()
+{
+    int n=0;
+    printf("enter n : ");
+    scanf("%d", &n);
+    return 0;
+}
